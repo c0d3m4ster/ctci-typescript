036678c58be51bb39298337bf9483149bd7f1f7c
Soudeh <babaeizadeh.s@gmail.com>
2024-09-27 12:50:54 +0330

    arrays and strings: about urlify

---
+++ arrays-and-strings/about-urlify/src/aboutUrlify.ts
@@ -0,0 +1,26 @@
+//  URLify: 
+//  Write a method to replace all spaces in a string with '%20'. 
+//  You may assume that the string has sufficient space at the end 
+//  to hold the additional characters, and that you are given the "true"
+//  length of the string. 
+//  (Note: If implementing in Java, please use a character array so that you can
+//  perform this operation in place.)
+//  EXAMPLE
+//  
+//  Input: "Mr John Smith    ", 13
+//  Output:"Mr%20John%20Smith"
+
+export function aboutUrlify(inputString: string, inputLength: number): string {
+    let output: string[] = [];
+    
+    for (let i = 0; i < inputLength; i++) {
+        const c = inputString[i];
+        if (c === ' ') {
+            output.push('%20');
+        } else {
+            output.push(c);
+        }
+    }
+
+    return output.join('');
+}
